1249917ae5d00555b82c8bb9bdd31dcb8f7bd8f5
EgorZaharov <00zaxar00@mail.ru>
2022-07-10 16:47:31 +0300

    HW#3 _ Task - 2 - correct 1

--- #3 Урок/HW#3_Task_2.py
+++ #3 Урок/HW#3_Task_2.py
@@ -1,10 +1,9 @@
 def translate_with_en_on_ru(translation):
     from translate import Translator
-    translator= Translator(to_lang="ru")
+    translator= Translator(from_lang = 'en', to_lang = 'ru')
     translation = translator.translate(input())
     return translation
 
 translation = 0
 translation=translate_with_en_on_ru(translation)
 print(translation)
-
