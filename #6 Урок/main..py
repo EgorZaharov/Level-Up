004552a7fef0fa0040977ac31d9ef6794bf49b9b
EgorZaharov <00zaxar00@mail.ru>
2022-07-15 20:51:23 +0300

    task_2

--- #6 Урок/main..py
+++ #6 Урок/main..py
@@ -1,14 +1,27 @@
 import json
 
-answer_1 = input("Как тебя зовут?")
-answer_2 = input("Сколько тебе лет?")
-answer_3 = input("Есть ли у тебя собака?")
+answer_1 = input("Третья планета от Солнца?")
+answer_2 = input("Самая яркая звезда?")
+answer_3 = input("Планета с кольцами?")
 
-r = [["Как тебя зовут?",answer_1], ["Сколько тебе лет?"],answer_2, ["Есть ли у тебя собака?"],answer_3]
+r = [["Третья планета от Солнца?",answer_1], ["Самая яркая звезда?"],answer_2, ["Планета с кольцами?"],answer_3]
 
 with open('json_data.json', 'w', encoding="utf8") as outfile:
     json.dump(r, outfile)
 
 with open('json_data.json', encoding="utf8") as json_file:
     r = json.load(json_file)
-    print(r)
+    if answer_1 == "Земля":
+        q = 1
+    else:
+        q = 0
+    if answer_2 == "Сириус":
+        q1 = 1
+    else:
+        q1 = 0
+    if answer_2 == "Сатурн":
+        q2 = 1
+    else:
+        q2 = 0
+    q_sum = q + q1 + q2
+    print(q_sum)
