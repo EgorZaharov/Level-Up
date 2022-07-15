76e74cbd8e4a3bbe844f266a101870cae869102d
EgorZaharov <00zaxar00@mail.ru>
2022-07-15 21:13:24 +0300

    task_3

--- #6 Урок/main..py
+++ #6 Урок/main..py
@@ -4,13 +4,35 @@ answer_1 = input("Третья планета от Солнца?")
 answer_2 = input("Самая яркая звезда?")
 answer_3 = input("Планета с кольцами?")
 
-r = [["Третья планета от Солнца?",answer_1], ["Самая яркая звезда?"],answer_2, ["Планета с кольцами?"],answer_3]
+r = [["Третья планета от Солнца?",answer_1], ["Самая яркая звезда?",answer_2], ["Планета с кольцами?",answer_3]]
+t = dict(r)
 
-with open('json_data.json', 'w', encoding="utf8") as outfile:
-    json.dump(r, outfile)
+# with open('json_data.json', 'w', encoding="utf8") as outfile:
+#     json.dump(t, outfile)
+#
+# with open('json_data.json', encoding="utf8") as json_file:
+#     t = json.load(json_file)
+#     if answer_1 == "Земля":
+#         q = 1
+#     else:
+#         q = 0
+#     if answer_2 == "Сириус":
+#         q1 = 1
+#     else:
+#         q1 = 0
+#     if answer_3 == "Сатурн":
+#         q2 = 1
+#     else:
+#         q2 = 0
+#     q_sum = q + q1 + q2
+#     print(q_sum)
 
-with open('json_data.json', encoding="utf8") as json_file:
-    r = json.load(json_file)
+import pickle
+
+with open('answer.pickle', 'wb') as file:
+    pickle.dump(t, file)
+
+with open('answer.pickle', 'rb') as file:
     if answer_1 == "Земля":
         q = 1
     else:
@@ -19,9 +41,10 @@ with open('json_data.json', encoding="utf8") as json_file:
         q1 = 1
     else:
         q1 = 0
-    if answer_2 == "Сатурн":
+    if answer_3 == "Сатурн":
         q2 = 1
     else:
         q2 = 0
     q_sum = q + q1 + q2
-    print(q_sum)
+    k = pickle.load(file)
+    print(k, f"Ваш результат: {q_sum} бала")
