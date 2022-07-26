42b0ada8f33d12a5dd863d86831b7aaf8b5f0bd5
EgorZaharov <00zaxar00@mail.ru>
2022-07-26 19:53:54 +0300

    Lesson_9 - Task 2

--- #9 Урок/Lesson_9.py
+++ #9 Урок/Lesson_9.py
@@ -1,8 +1,23 @@
-a = [1, 2, 3, 4, 5, 6]
+# #Task 1
+# a = [1, 2, 3, 4, 5, 6]
+#
+# b = a.index(max(a))
+# c = a.index(min(a))
+# a[b],a[c] = a[c],a[b]
+#
+# print(a)
 
-b = a.index(max(a))
-c = a.index(min(a))
-a[b],a[c] = a[c],a[b]
+# Task 2
 
+def isSublist(x, y):
+    if set(y) <= set (x):
+        return True
+    else:
+        return False
+
+x = list(input())
+y = list(input())
+a = isSublist(x, y)
 print(a)
+
 
